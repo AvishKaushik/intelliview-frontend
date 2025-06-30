6daad0b80074b20b049cebf0633085ac9fce5768
Avish Kaushik <avish.kaushik20@gmail.com>
2025-07-01 00:24:38 +0530

    test file deleted

--- test-env.ts
+++
@@ -1,5 +0,0 @@
-require('dotenv').config();
-
-console.log("AWS Access Key:", process.env.AWS_ACCESS_KEY_ID);
-console.log("AWS Secret Key:", process.env.AWS_SECRET_ACCESS_KEY);
-console.log("AWS Region:", process.env.AWS_REGION);
